8c589946a52ee6c26c819218a82466e2ae52dfc7
Samay Bhavsar <samay3191@gmail.com>
2025-03-19 20:24:14 +1100

    Added authentication

--- app.js
+++ app.js
@@ -5,11 +5,32 @@ const initializeDatabase = require("./database/initializeDatabase");
 const weatherStationsRoute = require("./routes/weatherStationsRoute");
 const variablesRoute = require("./routes/variablesRoute");
 const weatherDataRoute = require("./routes/weatherDataRoute");
+const { auth } = require("express-oauth2-jwt-bearer");
+require("dotenv").config();
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+
+// configuring auth0
+const jwtCheck = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+  tokenSigningAlg: process.env.AUTH0_SINGING_ALGORITHM,
+});
+
+// enforce on all endpoints
+app.use(jwtCheck);
+
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).json({
+      message: "Unauthorized: Invalid token or missing credentials.",
+    });
+  }
+  next(err);
+});
 
 // Initialize Database (Create Tables & Insert Default Data)
 initializeDatabase();
